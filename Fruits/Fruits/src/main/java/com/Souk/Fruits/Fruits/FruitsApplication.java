43eea108059366c0d3f6fab62ebdc1be3b57e0b2
majdibarkallah9 <113852585+majdibarkallah9@users.noreply.github.com>
2022-11-01 01:57:42 +0100

    Add files via upload

---
+++ Fruits/Fruits/src/main/java/com/Souk/Fruits/Fruits/FruitsApplication.java
@@ -0,0 +1,13 @@
+package com.Souk.Fruits.Fruits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsApplication.class, args);
+	}
+
+}
---
+++ Fruits/Fruits/src/test/java/com/Souk/Fruits/Fruits/FruitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Souk.Fruits.Fruits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
